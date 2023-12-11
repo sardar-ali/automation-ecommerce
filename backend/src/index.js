4ffc86158aea9878590b7f74d7196c9553765c4c
root <root@ubuntu-cloudpanel.localhost>
2023-12-11 11:22:26 +0000

    changes done

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,6 +29,6 @@ app.use("/api/v1/user", userRoutes)
 app.use("/api/v1/category", categoryRoutes)
 app.use("/api/v1/product", productRoutes)
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log("Server is running on port ", PORT)
 })
--- client/services/index.js
+++ client/services/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:4242/api/v1"
+const BASE_URL = "http://178.16.138.60:4242/api/v1"
 
 // auth urls 
 export const USER_LOGIN = `${BASE_URL}/user/login`;

4c86617273bf0ea5d597833e8fd4673bc0b05757
sardar-ali <alisardar465@gmail.com>
2024-01-11 18:58:20 +0400

    change meta title

--- client/pages/categories/[name].jsx
+++ client/pages/categories/[name].jsx
@@ -24,27 +24,27 @@ function CategoryScreen({ productsData }) {
                     key="desc"
                 />
                 <link href="https://images.search.yahoo.com/images/view;_ylt=Awriiu6pSoVl5jgHXLiJzbkF;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzk4MTYyYmJmNzVmN2MxN2JkNzQzMWQxODUzZjAzMTdjBGdwb3MDMzMEaXQDYmluZw--?back=https%3A%2F%2Fimages.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dsliding%2Bgate%252C%2Bgate%2Bbarrier%252C%2Bremote%2Bcontrol%252C%2Bswing%2Bgate%2Bin%2Bpicture%26ei%3DUTF-8%26type%3DE210US91215G0%26fr%3Dmcafee%26fr2%3Dp%253As%252Cv%253Ai%252Cm%253Asb-top%26tab%3Dorganic%26ri%3D33&w=1120&h=539&imgurl=www.chinaautogate.com%2Fphoto%2Fpl3249519-wireless_remote_control_automatic_boom_gates_fence_arm_barrier.jpg&rurl=http%3A%2F%2Fwww.chinaautogate.com%2Fsale-2725546-wireless-remote-control-automatic-boom-gates-fence-arm-barrier.html&size=98.5KB&p=sliding+gate%2C+gate+barrier%2C+remote+control%2C+swing+gate+in+picture&oid=98162bbf75f7c17bd7431d1853f0317c&fr2=p%3As%2Cv%3Ai%2Cm%3Asb-top&fr=mcafee&tt=Wireless+Remote+Control+Automatic+Boom+Gates+Fence+Arm+Barrier&b=0&ni=140&no=33&ts=&tab=organic&sigr=08hL4rmBfnW6&sigb=kMuqRNye6ekT&sigi=gdOWgaBDpBGA&sigt=hDjsyTYZUoMT&.crumb=mL1hVfn8LnJ&fr=mcafee&fr2=p%3As%2Cv%3Ai%2Cm%3Asb-top&type=E210US91215G0" rel="icon" />
-                <title>{productsData[0]?.category?.name ? productsData[0]?.category?.name : "Gate Automation"}</title>
+                <title>{productsData[0]?.category?.meta_title ? productsData[0]?.category?.meta_title : "Gate Automation"}</title>
             </Head>
             <Products
                 isCategory={true}
                 productsData={productsData}
             />
             {productsData[0]?.category ?
-                <div className="container-fluid pt-5 bg-white mx-auto" style={{display:"flex", justifyContent:"center", alignItem:"center",  maxWidth:"90%"}}>
+                <div className="container-fluid pt-5 bg-white mx-auto" style={{ display: "flex", justifyContent: "center", alignItem: "center", maxWidth: "90%" }}>
                     <div className="row px-xl-5 pb-3 mx-2">
                         <h3 className="mx-auto text-info" style={{ textAlign: "center" }}> {productsData[0]?.category?.name}</h3>
                         <div>
-                        {productsData[0]?.category?.content}
+                            {productsData[0]?.category?.content}
                         </div>
                     </div>
                 </div>
                 :
-                <div className="container-fluid pt-5 bg-white mx-2" style={{display:"flex", justifyContent:"center", alignItem:"center", margin:" 0 5rem"}}>
+                <div className="container-fluid pt-5 bg-white mx-2" style={{ display: "flex", justifyContent: "center", alignItem: "center", margin: " 0 5rem" }}>
                     <div className="row pb-3 mx-2">
                         <h3 className="mx-auto text-info" style={{ textAlign: "center" }}> {productsData[0]?.category?.name}</h3>
                         <div>
-                        Content Not Available
+                            Content Not Available
                         </div>
                     </div>
                 </div>
